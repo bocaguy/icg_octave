5be3d1542809df16ee412c8f94f4cc04a4c60d59
bocaguy <piano1202@gmail.com>
2015-08-29 19:13:06 +0000

    change to mysql

--- express_test.js
+++ express_test.js
@@ -1,52 +1,56 @@
+//EXPRESS:
+
 var express = require("express");
 var app = express();
+var flash    = require('connect-flash');
 
-//var logfmt = require("logfmt");
-//app.use(logfmt.requestLogger());
+//PASSPORT AUTHENTICATION:
 
-// Mongoose
-var mongoConnTest = 'mongodb://octave_user:#_Clobber8@ :48173,iad-c11-1.objectrocket.com:48173/db1'
-var mongoose = require('mongoose');
-var dbServer = 'Test';
-var Schema = mongoose.Schema;
+var passport = require('passport');
+require('./config/passport')(passport);  // pass passport for configuration
 
-if (process.env.MONGOHQ_URL) 
+// CONFIGURATION:
+
+app.configure(function() 
 {
-  mongoose.connect(process.env.MONGOHQ_URL);
-  dbServer = 'Prod';
-}
-else 
-{
-  mongoose.connect(mongoConnTest);
-  dbServer = 'Test';
-}
+ 
+  //  express application configuration
+  app.use(express.logger('dev'));  // log every request to the console
+  app.use(express.cookieParser());  // read cookies need for the authenticatiom
+  app.use(express.bodyParser());  // get information from html forms
 
-var schemaDealInfo = new Schema({
-  Client: String,
-  Deal: String,
-  LoanCount: Number
-}, 
-  { collection : 'DealInfo' }
-);
+  app.set('view  engine', 'ejs'); // set up ejs for templating
 
-var deal = mongoose.model('DealInfo', schemaDealInfo);
+  //  required for passport
+  app.use(express.session({  secret: 'mindstickarticles' })); //create sessin mindsticarticle
+  app.use(passport.initialize());  // passport intialize
+  app.use(passport.session());  // persistent login sessions of passport
+  app.use(flash());  // use connect-flash for flash messages stored in session
 
-app.get('/', function(req, res) 
-{
-      deal.find({ Deal: 'ACE' }, function(err, dealFound) 
-      {
-        if (err) throw err;
-        res.send(dbServer + ":" + dealFound);
-      });
 });
 
+// ROUTES:
+
+require('.//routes.js')(app,  passport); // load our routes and pass in our app and fully configured  passport
+
+// MONGOOSE/MONGODB:
+
+var mongoose = require('mongoose');
+var configDB =  require('./config/database.js');
+mongoose.connect(configDB.connectionString); //  connect to our database
+
+
+// POSTGRESQL/SEQUELIZE:
+
+var dbPG = require('./config/postgres.js');
+
+// START NODE.JS SERVER (Express)
+
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Server Loaded On " + port);
   console.log("IP Address Is: " + process.env.IP);
-  console.log("Environment Is: " + dbServer);
-  if (process.env.MONGOHQ_URL) 
-  {
-      console.log("Database is: " + dbServer);
-  }
+  console.log("Database Server: " + configDB.dbServer);
 });
+
